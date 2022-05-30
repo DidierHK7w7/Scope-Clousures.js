aa0a25ade08115cadd18f29ee7a14671e79049e1
Didier Hobak <dual.didier.hobak@gmail.com>
2022-05-30 16:45:51 -0500

    11.- Debugging

---
+++ src/debug/index.js
@@ -0,0 +1,12 @@
+//Global Scope
+var a = "Hello";
+function hello(){
+    let b = "hello world";
+    const c = "Hello World";
+    if (true) {
+        let d = "Hello World!!";
+        debugger
+    }
+}
+
+hello();
